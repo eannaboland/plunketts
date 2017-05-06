56be84a47bb92f0723c4a966f9ced14f196289cd
eannaboland <eannaboland@gmail.com>
2017-05-06 16:01:57 +0100

    Initial Commit

---
+++ Plunketts2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Plunketts2
+//
+//  Created by Eanna Boland on 06/05/2017.
+//  Copyright © 2017 Eanna Boland. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
