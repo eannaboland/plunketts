a471ba449f01ed8eb5c87a7000d07feb9c52c87e
eannaboland <eannaboland@gmail.com>
2017-05-16 21:36:14 +0100

    Committing first set of changes. Too many to mention. App icon, twitter feed and tab icons working

--- Plunketts2/SecondViewController.swift
+++ Plunketts2/SecondViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var videoview: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        videoview.allowsInlineMediaPlayback = true
+        
+        videoview.loadHTMLString("<iframe src=\"https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fplunketts%2Fvideos%2F1178501438892062%2F&show_text=0&width=560\" width=\"\(videoview.frame.width)\" height=\"\(videoview.frame.height)\" style=\"border:none;overflow:hidden\" scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\" allowFullScreen=\"true\"></iframe>", baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
